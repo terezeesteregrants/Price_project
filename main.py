9d51f88a2a881509811eb38e0ab974b2db4babab
terezeesteregrants <tereze-estere.grants@edu.rtu.lv>
2024-01-17 23:02:10 +0200

    atrod cenu no pirmās mājaslapas

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ def send_email(subject, message):
     server.sendmail(email, receiver_email, text.encode('utf-8'))
     print("Epasts tika nosūtīts uz " + receiver_email)
     server.quit()
+
+def check_price():
+    service = Service()
+    option = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=option)
+
+    
+    # pirmā mājaslapa
+    url1 = "https://www.tet.lv/veikals/plansetdatori/apple-ipad-10-9-wi-fi-64gb-blue-10th-gen-2022.html"
+    driver.get(url1)
+    time.sleep(2)
+    price_element1 = driver.find_element(By.CLASS_NAME, "i-product-data__price-inner")
+    price1 = float(price_element1.text.replace("€", "").replace(",", "."))
